94bf9579141d547fd017296b1183ed6c0f8b58e9
Rômulo Soares <romulosoares@ice.ufjf.br>
2022-01-09 15:02:47 -0300

    Add get by name

--- src/routes/agentRoutes.py
+++ src/routes/agentRoutes.py
@@ -23,14 +23,26 @@ async def show_agent(idAgent: str):
     if (agent := await database.db["agent"].find_one({"_id": idAgent})) is not None:
         return agent
 
-    raise HTTPException(status_code=404, detail=f"Agent {id} not found")
+    raise HTTPException(status_code=404, detail=f"Agent {idAgent} not found")
+
+@router.get("/get/name/{nameAgent}}", response_description="Get a single agent by name", response_model=provenanceModel.agentModel)
+async def show_agent_name(nameAgent: str):
+    if (agent := await database.db["agent"].find_one({"name": nameAgent})) is not None:
+        return agent
+
+    raise HTTPException(status_code=404, detail=f"Agent {nameAgent} not found")
 
 @router.post("/post/", response_description="Add new agent", response_model=provenanceModel.agentModel)
 async def create_agent(agent: provenanceModel.agentModel = Body(...)):
     agent = jsonable_encoder(agent)
-    new_agent = await database.db["agent"].insert_one(agent)
-    created_agent = await database.db["agent"].find_one({"_id": new_agent.inserted_id})
-    return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_agent)
+    
+    if (agent := await database.db["agent"].find_one({"name": agent["name"]})) is not None:
+        print("Already exists")
+        return JSONResponse(status_code=status.HTTP_409_CONFLICT, content={"error": "Already exists"})
+    else:
+        new_agent = await database.db["agent"].insert_one(agent)
+        created_agent = await database.db["agent"].find_one({"_id": new_agent.inserted_id})
+        return JSONResponse(status_code=status.HTTP_201_CREATED, content=created_agent)
 
 
 
